4de76aab8f4ff6b25005b2d2497b7319df80afac
Alexandre Kabla <ajk61@cam.ac.uk>
2023-01-22 05:13:16 +0000

    Add alternative DM4 format

--- 20230121_cfunction_funcwrap.jl
+++ 20230121_cfunction_funcwrap.jl
@@ -30,6 +30,17 @@ function DM3(p::Array{Symbol}, G::Expr)
       (Float64, Vector{Float64}))
 end
 
+
+function DM4(p::Array{Symbol}, G::Expr)
+    unpack_expr = Meta.parse(string(join(string.(p), ","), ",=params"))
+    anonf = @eval ((t, params) -> begin $unpack_expr; $G; end)
+    @cfunction(
+      $anonf,
+      Float64,
+      (Float64, Vector{Float64})).f
+end
+
+
 sls = quote k0 + k1*exp((-t*k1) / η) end
 
 fw0  = DM0([:k0, :k1, :η], sls)
@@ -37,6 +48,7 @@ fw1  = DM1([:k0, :k1, :η], sls)
 fw2  = DM2([:k0, :k1, :η], sls)
 fw3  = DM3([:k0, :k1, :η], sls)
 pfw3 = Base.unsafe_convert(Ptr{Cvoid}, fw3)
+fw4  = DM4([:k0, :k1, :η], sls)
 
 t = 1.0
 params_numbers = [2.0, 3.0, 5.0]
@@ -52,6 +64,7 @@ ccall(pfw3, Float64, (Float64, Vector{Float64}), t, params_numbers)
 @btime fw2($t, $params_numbers)
 @btime fw3.f($t, $params_numbers)
 @btime ccall($pfw3, Float64, (Float64, Vector{Float64}), $t, $params_numbers)
+@btime fw4($t, $params_numbers)
 
 function DM_expectfail(p::Array{Symbol}, G::Expr)
     unpack_expr = Meta.parse(string(join(string.(p), ","), ",=params"))
@@ -89,12 +102,13 @@ wrapped = thang()
 # used
 
 #= Results
-  262.908 ns (2 allocations: 32 bytes)
-  263.174 ns (2 allocations: 32 bytes)
-  263.205 ns (2 allocations: 32 bytes)
-  100.000 ns (2 allocations: 32 bytes)
-  58.359 ns (2 allocations: 32 bytes)
-  99.367 ns (4 allocations: 64 bytes)
+  168.115 ns (2 allocations: 32 bytes)
+  167.376 ns (2 allocations: 32 bytes)
+  171.422 ns (2 allocations: 32 bytes)
+  51.157 ns (2 allocations: 32 bytes)
+  31.122 ns (2 allocations: 32 bytes)
+  28.861 ns (2 allocations: 32 bytes)
+  51.294 ns (4 allocations: 64 bytes)
 =#
 
 # https://github.com/JuliaLang/julia/pull/32737
