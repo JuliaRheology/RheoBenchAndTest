2f6636661bd683f55522425c36d9fa4c4e92c2e0
Alexandre Kabla <ajk61@cam.ac.uk>
2023-01-22 06:37:55 +0000

    Added functions on time arrays

--- 20230121_cfunction_funcwrap.jl
+++ 20230121_cfunction_funcwrap.jl
@@ -91,6 +91,44 @@ f()
 
 
 
+# Tests on time arrays
+
+
+function DM1v(p::Array{Symbol}, G::Expr)
+    unpack_expr = Meta.parse(string(join(string.(p), ","), ",=params"))
+    return(
+    (@eval ((t, params) -> begin $unpack_expr; @. $G; end))
+    |> FunctionWrapper{Vector{Float64}, Tuple{Vector{Float64},Vector{Float64}}})
+end
+
+function DM5v(p::Array{Symbol}, G::Expr)
+    unpack_expr = Meta.parse(string(join(string.(p), ","), ",=params"))
+    anonf = @eval ((t, params) -> begin $unpack_expr; @. $G; end)
+    (t, params_numbers) -> ccall( @cfunction(  $anonf,  Vector{Float64}, (Vector{Float64}, Vector{Float64})).ptr , Vector{Float64}, (Vector{Float64}, Vector{Float64}), t, params_numbers)
+end
+
+ta=Array(0:0.1:10)
+fw1v  = DM1v([:k0, :k1, :η], sls)
+fw5v  = DM5v([:k0, :k1, :η], sls)
+fw1v(ta, params_numbers);
+fw5v(ta, params_numbers);
+
+@btime fw1v($ta, $params_numbers);
+@btime fw5v($ta, $params_numbers);
+
+# Reference for performance
+function fslsv(t,k)
+   k[1].+k[2].*exp.((.- t.*k[2])./k[3])
+end
+fslsv(ta, params_numbers);
+@btime fslsv($ta, $params_numbers);
+
+
+
+
+
+
+
 function DM_expectfail(p::Array{Symbol}, G::Expr)
     unpack_expr = Meta.parse(string(join(string.(p), ","), ",=params"))
     anonf = @eval ((t, params) -> begin $unpack_expr; $G; end)
@@ -127,14 +165,17 @@ wrapped = thang()
 # used
 
 #= Results
-  169.623 ns (2 allocations: 32 bytes)
-  167.598 ns (2 allocations: 32 bytes)
-  171.513 ns (2 allocations: 32 bytes)
-  51.449 ns (2 allocations: 32 bytes)
-  30.158 ns (2 allocations: 32 bytes)
-  27.826 ns (2 allocations: 32 bytes)
-  30.664 ns (2 allocations: 32 bytes)
-  51.280 ns (4 allocations: 64 bytes)
+  169.451 ns (2 allocations: 32 bytes)
+  173.386 ns (2 allocations: 32 bytes)
+  172.940 ns (2 allocations: 32 bytes)
+  51.171 ns (2 allocations: 32 bytes)
+  29.483 ns (2 allocations: 32 bytes)
+  27.946 ns (2 allocations: 32 bytes)
+  30.390 ns (2 allocations: 32 bytes)
+  742.040 ns (1 allocation: 896 bytes)
+  724.879 ns (1 allocation: 896 bytes)
+  711.021 ns (1 allocation: 896 bytes)
+  51.606 ns (4 allocations: 64 bytes)
 
 =#
 
